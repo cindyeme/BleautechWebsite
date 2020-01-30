d4d524b942aa2b4f279bfc665aac3eec60cfd8bf
rexsteroxy <rexsteroxy22@gmail.com>
2020-01-30 15:40:35 +0100

    implemented blog edit and delete functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -58,4 +58,53 @@ class AdminController extends Controller
             
            return redirect('/admin')->with('response','Published successfully');
     }
+
+    public function viewBlog()
+    {
+        $blogs = Blog::all();;
+    
+        return view('viewblogs',compact('blogs'));
+    }
+
+    public function deleteBlog($blog_id){
+        Blog::where('id',$blog_id)->delete();
+
+        return redirect('/admin')->with('response','Blog deleted  successfully');
+}
+
+public function editBlog(Request $request,$blog_id){
+
+    $this->validate($request, [
+        "blog_title"=>"required",
+        "image_url" => "required",
+        "story"=>"required"
+    ]);
+    $blog = new Blog;
+    $blog->user_id = Auth::user()->id;
+    $blog->blog_title = $request->input('blog_title');
+    $blog->image_Url = $request->input('image_url');
+    $blog->story = $request->input('story');
+        $data = array(
+            "user_id"=>$blog->user_id,
+            "blog_title"=>$blog->user_id,
+            "image_Url"=>$blog->image_Url,
+            "story"=>$blog->story
+        );
+        Blog::where('id',$blog_id)->update($data);
+        $blog->update();
+       return redirect('/admin')->with('response','Blog Edited  successfully');
+
+ return $blog_id;
+
+
+
+}
+
+public function viewEditBlog($blog_id)
+    {
+        $blog = Blog::find($blog_id);
+        return view('editblog',compact('blog'));
+    }
+
+
 }
